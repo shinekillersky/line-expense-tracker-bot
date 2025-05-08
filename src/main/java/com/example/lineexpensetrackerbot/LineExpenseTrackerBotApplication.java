3561fbd8f6dfba03a4bf7d816954d9ad02185773
小P <shinekillersky@users.noreply.github.com>
2025-05-08 21:32:42 +0800

    first commit

---
+++ src/main/java/com/example/lineexpensetrackerbot/LineExpenseTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.lineexpensetrackerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineExpenseTrackerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineExpenseTrackerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lineexpensetrackerbot/LineExpenseTrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lineexpensetrackerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineExpenseTrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
